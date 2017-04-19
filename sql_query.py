a5125ca64af0e563dd937ae74c76d0bb24b44315
StewartUF <stewart.wehmeyer@gmail.com>
2017-04-19 17:04:38 -0400

    add templates files for this repo
    config, sql query formatter, and emailer

---
+++ sql_query.py
@@ -0,0 +1,9 @@
+#Stewart Wehmeyer
+#4/19/17
+
+"""
+1. Get appropriate SQL query (select * from recommended_visits_with_scheduling_report_details) passed to python
+2. Put the query (result) into HTML table so it is legible
+3. Email query
+"""
+
